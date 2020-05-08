2f135d36f4339579e6dcae1d7e8297e05dc88fcc
Nickolay Polonskyi <nickolay.polonskyi@anodot.com>
2020-05-08 15:36:57 +0300

    Added config creator

---
+++ config_maker/config_creator.go
@@ -0,0 +1,306 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/manifoldco/promptui"
+)
+
+const green = "\u001b[32m"
+const red = "\u001b[31m"
+const reset = "\u001b[0m"
+
+var metrics = map[string][]string{
+	"EC2":        []string{"CoreCount", "VCpuCount", "Done"},
+	"EBS":        []string{"Size", "Done"},
+	"S3":         []string{"BucketSizeBytes", "NumberOfObjects", "Done"},
+	"Cloudfront": []string{"BytesDownloaded", "Requests", "TotalErrorRate", "Done"},
+}
+var services = []string{"EC2", "EBS", "S3", "Cloudfront", "Default (All services above)", "Done"}
+
+var regions = []string{
+	"eu-north-1",
+	"ap-south-1",
+	"eu-west-3",
+	"eu-west-2",
+	"eu-west-1",
+	"ap-northeast-2",
+	"ap-northeast-1",
+	"sa-east-1",
+	"ca-central-1",
+	"ap-southeast-1",
+	"ap-southeast-2",
+	"eu-central-1",
+	"us-east-1",
+	"us-east-2",
+	"us-west-1",
+	"us-west-2",
+}
+
+var selectedRegions []string
+
+type Metric struct {
+	name string
+}
+
+type Service struct {
+	name    string
+	metrics []Metric
+}
+
+type RegionConfig struct {
+	region   string
+	services []Service
+}
+
+func GetAllMetricsAllServices() []Service {
+	services := make([]Service, 0)
+	for s, m := range metrics {
+		metrics_ := make([]Metric, 0)
+		for _, metric := range m {
+			if metric == "Done" {
+				break
+			}
+			metrics_ = append(metrics_, Metric{name: metric})
+		}
+		services = append(services, Service{name: s, metrics: metrics_})
+	}
+	return services
+}
+
+func ValidateRegion(input string) error {
+	valid := false
+	for _, r := range regions {
+		if input == r {
+			valid = true
+			break
+		}
+	}
+	if !valid {
+		return fmt.Errorf("Please provide correct region name")
+	}
+
+	for _, sr := range selectedRegions {
+		if sr == input {
+			return fmt.Errorf("Region %s already selected please choose another one", input)
+		}
+	}
+	return nil
+}
+
+func CustomSelect(label string, items []string) (string, error) {
+	templates := &promptui.SelectTemplates{
+		Label:    "{{ . }}?",
+		Active:   " > {{ . | cyan }} ",
+		Inactive: "  {{ . | magenta }} ",
+		Selected: "", //"{{ . | red | cyan }}",
+	}
+	prompt := promptui.Select{
+		Label:     label,
+		Items:     items,
+		Templates: templates,
+	}
+
+	_, result, err := prompt.Run()
+
+	if err != nil {
+		return "", err
+	}
+	return result, nil
+}
+
+func CustomPrompt(validate func(string) error, name string) (string, error) {
+	prompt := promptui.Prompt{
+		Label:    name,
+		Validate: validate,
+	}
+	result, err := prompt.Run()
+
+	if err != nil {
+		return "", err
+	}
+	return result, nil
+}
+
+func removeDuplicates(list []string) []string {
+	new := make([]string, 0)
+	ifPresent := false
+	for _, s := range list {
+		for _, snew := range new {
+			if s == snew {
+				ifPresent = true
+			}
+		}
+		if !ifPresent {
+			new = append(new, s)
+		} else {
+			ifPresent = false
+		}
+	}
+	return new
+}
+
+func removeDuplicatesMetrics(list []Metric) []Metric {
+	new := make([]Metric, 0)
+	ifPresent := false
+	for _, s := range list {
+		for _, snew := range new {
+			if s.name == snew.name {
+				ifPresent = true
+			}
+		}
+		if !ifPresent {
+			new = append(new, s)
+		} else {
+			ifPresent = false
+		}
+	}
+	return new
+}
+
+func removeDuplicatesService(list []Service) []Service {
+	new := make([]Service, 0)
+	ifPresent := false
+	for _, s := range list {
+		for _, snew := range new {
+			if s.name == snew.name {
+				ifPresent = true
+			}
+		}
+		if !ifPresent {
+			new = append(new, s)
+		} else {
+			ifPresent = false
+		}
+	}
+	return new
+}
+
+func ListToString(list []string) string {
+	list = removeDuplicates(list)
+	var r string
+	for _, s := range list {
+		r = r + "\n\t" + green + s + reset
+	}
+	return r
+}
+
+func ChooseMetrics(service string) ([]Metric, error) {
+	metrics_ := make([]Metric, 0)
+	metricsname := make([]string, 0)
+	for {
+		metric, err := CustomSelect("Metric", metrics[service])
+		if err != nil {
+			return make([]Metric, 0), err
+		}
+		if metric == "Done" {
+			break
+		}
+
+		metricsname = append(metricsname, metric)
+		metrics_ = append(metrics_, Metric{name: metric})
+		fmt.Printf("You chose next metrics for service: %s", service)
+		fmt.Println(ListToString(metricsname))
+	}
+	return removeDuplicatesMetrics(metrics_), nil
+}
+
+func ChoseService(region string) ([]Service, error) {
+	chosenservices := make([]Service, 0)
+	servicenames := make([]string, 0)
+	for {
+
+		service, err := CustomSelect("Services", services)
+		if err != nil {
+			fmt.Printf("Prompt failed %v\n", err)
+		}
+
+		if service == "Done" {
+			break
+		}
+
+		if service == "Default (All services above)" {
+			return GetAllMetricsAllServices(), nil
+		}
+
+		metrics_, err := ChooseMetrics(service)
+		if err != nil {
+			return make([]Service, 0), err
+		}
+		chosenservices = append(chosenservices, Service{name: service, metrics: metrics_})
+		servicenames = append(servicenames, service)
+		fmt.Printf("You chose next services for region: %s", region)
+		fmt.Println(ListToString(servicenames))
+	}
+	return removeDuplicatesService(chosenservices), nil
+}
+
+func ChoseRegion() (string, error) {
+	result, err := CustomPrompt(ValidateRegion, "Region")
+	if err != nil {
+		return "", err
+	}
+	selectedRegions = append(selectedRegions, result)
+	return result, nil
+}
+
+func WriteConfig(data []byte) error {
+	fo, err := os.Create("output.yaml")
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err := fo.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	if _, err := fo.Write(data); err != nil {
+		return err
+	}
+	return nil
+}
+
+func main() {
+	configs := make([]RegionConfig, 0)
+
+	for {
+
+		fmt.Println("Hello please add region you need to monitor: ")
+		region, err := ChoseRegion()
+		if err != nil {
+			fmt.Printf("Error occured %v", err)
+			break
+		}
+		regionconfig := RegionConfig{region: region}
+
+		fmt.Println("Hello please choose services you need to monitor: ")
+		chosenservices, err := ChoseService(region)
+		if err != nil {
+			break
+		}
+		regionconfig.services = chosenservices
+		configs = append(configs, regionconfig)
+		prompt := promptui.Select{
+			Label: "Do you want do add another region ?",
+			Items: []string{"Yes", "No"},
+		}
+
+		_, newregion, err := prompt.Run()
+		if err != nil {
+			fmt.Printf("Prompt failed %v\n", err)
+			break
+		}
+		if newregion == "No" {
+			break
+		}
+	}
+	confstr := RenderConfig(configs)
+	fmt.Println(confstr)
+	if err := WriteConfig([]byte(confstr)); err != nil {
+		fmt.Println(err)
+		os.Exit(127)
+	}
+}
